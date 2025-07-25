c107bd4d6287ff11243c4570d602f0688969ab27
gollasachin12 <gollasachin12@gmail.com>
2025-07-25 11:14:47 +0530

    version

---
+++ html 55 R/Sachin.java
@@ -0,0 +1,10 @@
+class Sachin{
+    public static void main(String args[]){
+        int sum=0;
+        int i=1;
+        while(i<=10){
+            sum=sum+i;
+        }
+        System.out.println(sum);
+    }
+}
